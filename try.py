cc112b8c8773aa3661c5f32cd5339ab019db635f
orionfoysal <orionfoysal@gmial.com>
2018-01-22 23:48:42 +0600

    saves data to a csv

--- try.py
+++ try.py
@@ -1,36 +1,100 @@
+import math
 import re
+import sys
+import pdftotext
 
-with open("pytotext.txt") as f:
-	lines = f.readlines()
+filename = sys.argv[1]
+#Load the PDF
+with open(filename, "rb") as f:
+	pdf = pdftotext.PDF(f)
+
+# Iterate over all the pages
+doc = ""
+for page in pdf:
+	doc += page
+
+print(doc)
+
+lines = doc.split('\n')
 
 lineNumber = 0
+
+nameOne = lines[2].lstrip()
+# print(nameOne)
+
+reference = math.nan 
+
 for line in lines:
 	if lineNumber < 10:
 		if line.find('Mobile') >= 0:
-			mob = line.split(':')[1]
-			print(mob)
+			mobOne = line.split(':')[1].lstrip()
+			# print(mobOne)
 		if line.find('email') >= 0:
-			email = line.split(':')[1]
-			print(email)
+			emailOne = line.split(':')[1].lstrip()
+			# print(emailOne)
 	if line.find('Reference ') >= 0:
-		print(lineNumber)
-		
-		name = re.split(': | \t', lines[lineNumber + 2])[1]
-		organization = re.split(':', lines[lineNumber + 3])[1]
-		designation = re.split(': | \t', lines[lineNumber + 4])[1]
-		mobile = re.split(': | \t', lines[lineNumber + 8])[1]
-#		email = re.split(': | \t', lines[lineNumber + 9])[1]
-		print(re.split(r'\s{2,}', name))
-		print(re.split(r'\s{2,}', organization))
-		print(re.split(r'\s{2,}', designation))
-		print(re.split(r'\s{2,}', mobile))
-#		print(re.split(r'\s{2,}', email))
-		print(lines[lineNumber + 2].split(":")[0])
+		reference = lineNumber
+	
+	if lineNumber > reference:
+		try:
+			if line.find('Mobile') >= 0:
+				mobile = re.split(': | \s{2,}', line)
+				mobTwo = mobile[2]
+				mobThree = mobile[3]
+				# print(mobTwo)
+				# print(mobThree)
+
+			elif line.find('EMail') >= 0:
+				email = re.split(': | \s{2,}', line)
+				emailTwo = email[2]
+				emailThree = email[3]
+				# print(emailTwo)
+				# print(emailThree)
+			
+			elif line.find('Organization') >= 0:
+				organization = re.split(': | \s{2,}', line)
+				# print(organization)
+				orgTwo = organization[2]
+				orgThree = organization[3]
+				# print(orgTwo)
+				# print(orgThree)
+
+			elif line.find('Designation') >= 0:
+				designation = re.split(': | \s{2,}', line)
+				desigTwo = designation[2]
+				desigThree = designation[3]
+				# print(desigTwo)
+				# print(desigThree)
+
+			elif line.find('Name') >= 0:
+				name = re.split(':|\s{2,}', line)
+				nameTwo = name[2].lstrip()
+				nameThree = name[3].lstrip()
+				# print(nameThree)
+				# print(nameTwo)
+
+		except:
+			pass
 
 	if line.find('Employment History') >= 0:
 		job = lines[lineNumber + 2 : lineNumber + 6]
-		print(job)
+		desigOne = ''.join(job)
+		# print(job)
 	
 	lineNumber += 1
 
+# print(lines	)
+orgOne = ""
+infoOne = nameOne +',' + emailOne + ',' + mobOne + ',' + desigOne + ',' + orgOne
+infoTwo = nameTwo + ',' + emailTwo + ',' + mobTwo + ',' + desigTwo + ',' + orgTwo
+infoThree = nameThree + ',' + emailThree + ',' + mobThree + ',' + desigThree + ',' + orgThree
 
+
+print(infoOne)
+print(infoTwo)
+print(infoThree)
+
+with open('test.csv','w') as f:
+	f.write(infoOne+'\n')
+	f.write(infoTwo + '\n')
+	f.write(infoThree + '\n')
